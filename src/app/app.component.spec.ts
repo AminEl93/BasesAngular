c239ee148224560755312bb22b96b10c39fd097f
AminEl93 <amin_11_iverson@hotmail.com>
2023-09-22 18:05:06 +0200

    Cambios de configuración

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -12,16 +12,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'BasesAngular'`, () => {
+  it(`should have as title 'bases-angular'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('BasesAngular');
+    expect(app.title).toEqual('bases-angular');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('BasesAngular app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('bases-angular app is running!');
   });
 });
