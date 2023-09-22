a9ae0c42e76fffe1e0824c79d394ff745b77572e
AminEl93 <amin_11_iverson@hotmail.com>
2023-09-22 10:42:52 +0200

    Renombramiento del proyecto (definitivo)

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -12,16 +12,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'CursoAngular'`, () => {
+  it(`should have as title 'BasesAngular'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('CursoAngular');
+    expect(app.title).toEqual('BasesAngular');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('CursoAngular app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('BasesAngular app is running!');
   });
 });
