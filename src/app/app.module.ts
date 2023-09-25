02efa983e761c163158d18e9a1ef181e021d2d53
AminEl93 <amin_11_iverson@hotmail.com>
2023-09-25 13:47:38 +0200

    Módulo DBZ : Servicio, Lista de personajes...

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { CounterModule } from './counter/counter.module';
 import { HeroesModule } from './heroes/heroes.module';
+import { DbzModule } from './dbz/dbz.module';
 
 import { AppComponent } from './app.component';
 
-
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    CounterModule,
-    HeroesModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        CounterModule,
+        HeroesModule,
+        DbzModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
+
 export class AppModule { }
---
+++ src/app/dbz/components/list/list.component.ts
@@ -0,0 +1,26 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Character } from '../../interfaces/character.interface';
+
+
+@Component({
+  selector: 'dbz-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.css']
+})
+
+export class ListComponent {
+
+    @Input()
+    public characterList: Character[] = [{   // Personaje por defecto
+        name: 'Trunks',
+        power: 10
+    }];
+    
+    @Output() public onDelete: EventEmitter<string> = new EventEmitter();
+    // onDelete = Index value : number
+
+    onDeleteCharacter(id?: string): void {
+        if (!id) return;
+        this.onDelete.emit(id);
+    }
+}
